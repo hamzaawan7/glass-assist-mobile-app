ed68ce39a76e88e81985644fea176234e3568573
“Umar-Farooq-Shafi” <umarfarooqshafi2101@gmail.com>
2024-02-27 22:50:14 +0500

    expo-background-fetch

--- App.js
+++ App.js
@@ -5,11 +5,44 @@ import { ThemeProvider, Provider as PaperProvider } from "react-native-paper";
 import { RootSiblingParent } from "react-native-root-siblings";
 import NetInfo from '@react-native-community/netinfo';
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
 
 import 'react-native-get-random-values'
 
 import Stack from "./src/navigation/Stack";
 import instance from "./src/api/axios";
+
+const BACKGROUND_FETCH_TASK = 'background-fetch';
+
+// 1. Define the task by providing a name and the function that should be executed
+// Note: This needs to be called in the global scope (e.g outside of your React components)
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  const now = Date.now();
+
+  console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
+
+  // Be sure to return the successful result type!
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
+
+// 2. Register the task at some point in your app by providing the same name,
+// and some configuration options for how the background fetch should behave
+// Note: This does NOT need to be in the global scope and CAN be used in your React components!
+async function registerBackgroundFetchAsync() {
+  return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+    minimumInterval: 60 * 15, // 15 minutes
+    stopOnTerminate: false, // android only,
+    startOnBoot: true, // android only
+  });
+}
+
+// 3. (Optional) Unregister tasks by specifying the task name
+// This will cancel any future background fetch calls that match the given name
+// Note: This does NOT need to be in the global scope and CAN be used in your React components!
+async function unregisterBackgroundFetchAsync() {
+  return BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
+}
 
 export default function App() {
   const [initialScreen, setInitialScreen] = useState();
@@ -17,6 +50,7 @@ export default function App() {
 
   useLayoutEffect(() => {
     setIsLoading(true);
+    checkStatusAsync();
 
     const unsubscribe = NetInfo.addEventListener(state => {
       if (!state.isConnected) {
@@ -48,7 +82,24 @@ export default function App() {
     return () => {
       unsubscribe();
     }
-  }, [initialScreen])
+  }, [initialScreen]);
+
+  const checkStatusAsync = async () => {
+    const status = await BackgroundFetch.getStatusAsync();
+    const isRegistered = await TaskManager.isTaskRegisteredAsync(BACKGROUND_FETCH_TASK);
+    console.log(status);
+    console.log(isRegistered);
+  };
+
+  const toggleFetchTask = async () => {
+    if (isRegistered) {
+      await unregisterBackgroundFetchAsync();
+    } else {
+      await registerBackgroundFetchAsync();
+    }
+
+    checkStatusAsync();
+  };
 
   if (isLoading) {
     return (
